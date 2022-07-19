13477dd64e3c9740b1dcbc13586f7a96a6e92fba
saltyy14 <aleks98.02@mail.ru>
2022-07-19 20:31:45 +0300

    task1-array.js

--- tasks/task1-array.js
+++ tasks/task1-array.js
@@ -8,44 +8,9 @@
  */
 let sarray = [{width: 10, height: 10}, {width: 5, height: 30}, {width: 40, height: 2}];
 
-function quicksort(array) {
-    if (array.length <= 1) {
-        return array;
-    }
-    
-    let pivot = ( array[0].height * array[0].width );
-    
-    let left = []; 
-    let right = [];
-    
-    for (let i = 1; i < array.length; i++) {
-        ( array[i].height * array[i].width ) > pivot ? left.push(array[i]) : right.push(array[i]);
-    }
-    return quicksort(left).concat(array[0], quicksort(right));
-  }
-
-function turnIntoSquares(array) {
-    let sarray1 = [];
-
-    function calcSquare( width, height ) {
-        return width * height;
-    }
-    
-    for ( let i = 0; i < array.length; i++ ) {
-        let s1 = calcSquare( array[i].width, array[i].height );
-        sarray1[i] = s1;
-    }
-    return sarray1;
-}
-
-function sortBySquare(array) {
-   let sarray2 = quicksort(array);
-
-   for ( let i = 0; i < sarray2.length; i++ ) {
-        console.log( 'width:' + sarray2[i].width + ' height:' + sarray2[i].height);
-    }
-    return sarray2;
-}
-
-console.log ( turnIntoSquares( sarray ) );
-sortBySquare( sarray );
+let turnIntoSquares = (array) => array.map((x) => x.width * x.height);
+ 
+let sortBySquare = (array) => array.sort((a,b) => b.width * b.height - a.width * a.height);
+     
+console.log (turnIntoSquares(sarray));
+console.log (sortBySquare(sarray));
